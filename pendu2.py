bc7abdf3ee92e73cc873d6ea0644458c8a85cd08
NacelisMircol <65544231+NacelisMircol@users.noreply.github.com>
2021-03-05 22:29:11 +0100

    Ordinateur qui devine le mot

--- pendu2.py
+++ pendu2.py
@@ -114,69 +114,72 @@ def proposition_lettre(occurence_lettres, lettres_proposees):#Fonction qui chois
 #--------- BOUCLE PRINCIPALE ----------
 while perdu==False and trouve==False:
 
-	os.system('clear')
+    os.system('clear')
 
-	print(dessinPendu(etat_pendu))
+    print(dessinPendu(etat_pendu))
 
-	for i in mot_final:#Affichage du mot dans la console
-		if i == None:
-			print("_", end=" ")
-		else:
-			print(i, end = " ")
-	print("")
+    for i in mot_final:#Affichage du mot dans la console
+        if i == None:
+            print("_", end=" ")
+        else:
+            print(i, end = " ")
+    print("")
 
-	for i in range(len(mot_final)):
-		print(str(i+1), end=" ")
+    for i in range(len(mot_final)):
+        print(str(i+1), end=" ")
 
-	print("\n")
+    print("\n")
 
-	for lettre in alphabet: #On remet à 0 le dictionnaire
-		occurence_lettres[lettre] = 0
+    for lettre in alphabet: #On remet à 0 le dictionnaire
+        occurence_lettres[lettre] = 0
 
-	for mot in liste_mots_possibles:#On remplit le dictionnaire avec les nouvelles valeurs
-		for lettre in mot:
-			try:
-				occurence_lettres[lettre]+=1
-			except:
-				pass
+    for mot in liste_mots_possibles:#On remplit le dictionnaire avec les nouvelles valeurs
+        for lettre in mot:
+            try:
+                occurence_lettres[lettre]+=1
+            except:
+                pass
 
 
-	lettre_proposee = proposition_lettre(occurence_lettres, lettres_proposees)#On récupère dans la variable lettre_proposee la lettre la plus adaptée à proposer
+    lettre_proposee = proposition_lettre(occurence_lettres, lettres_proposees)#On récupère dans la variable lettre_proposee la lettre la plus adaptée à proposer
 
-	resultat = input("En quelle position le mot comporte un "+ lettre_proposee+" ? ")
+    resultat = input("En quelle position le mot comporte un "+ lettre_proposee+" ? ")
 	#resultat = str(resultat)
 
 	#----On supprime de la liste tous les mots qui ne correspondent plus----
-	nb_mots_supprimes =0
-	if resultat == "0":#Le mot ne comporte pas cette lettre, on enlève tous les mots qui ont cette lettre
-		etat_pendu +=1
-		for i in range(len(liste_mots_possibles)):
-			if lettre_proposee in liste_mots_possibles[i-nb_mots_supprimes]:
-				liste_mots_possibles.pop(i-nb_mots_supprimes)
-				nb_mots_supprimes+=1
-	else:#On a donné la/les position(s) de la lettre dans le mot. On peut donc supprimer les mots qui ne correspondent pas
-		resultat = resultat.split(",")
-		for i in resultat:
-			nb_mots_supprimes = 0
-			index = int(i)-1
+    nb_mots_supprimes =0
+    if resultat == "0":#Le mot ne comporte pas cette lettre, on enlève tous les mots qui ont cette lettre
+        etat_pendu +=1
+        for i in range(len(liste_mots_possibles)):
+            if lettre_proposee in liste_mots_possibles[i-nb_mots_supprimes]:
+                liste_mots_possibles.pop(i-nb_mots_supprimes)
+                nb_mots_supprimes+=1
+    else:#On a donné la/les position(s) de la lettre dans le mot. On peut donc supprimer les mots qui ne correspondent pas
+        resultat = resultat.split(",")
+        for i in resultat:
+            nb_mots_supprimes = 0
+            index = int(i)-1
             #print(type(mot))
-			mot_final[index] = lettre_proposee
-			for i in range(len(liste_mots_possibles)):
-				if lettre_proposee != liste_mots_possibles[i-nb_mots_supprimes][index]:
-					liste_mots_possibles.pop(i-nb_mots_supprimes)
-					nb_mots_supprimes+=1
-	lettres_proposees.append(lettre_proposee) #On ajoute la lettre à la liste pour ne pas la proposer une nouvelle fois
+            mot_final[index] = lettre_proposee
+            for i in range(len(liste_mots_possibles)):
+                if lettre_proposee != liste_mots_possibles[i-nb_mots_supprimes][index]:
+                    liste_mots_possibles.pop(i-nb_mots_supprimes)
+                    nb_mots_supprimes+=1
+    lettres_proposees.append(lettre_proposee) #On ajoute la lettre à la liste pour ne pas la proposer une nouvelle fois
 
 
 
 
 	#----On vérifie si on a terminé la partie----
-	if etat_pendu>=7:
-		perdu = True
-		print("L\'ordinateur n'a pas trouvé, le pendu est mort avant...")
-	if None not in mot_final:#Il y a plus de lettres inconnues dans le mot donc on a gagné
-		print("Trouvé ! Le mot était "+"".join(mot_final))
-		trouve = True
-	if len(liste_mots_possibles)==0 and trouve==False:#Le mot qu'on cherche n'est visiblement pas dans le dictionnaire, on a donc perdu
-		print("L'ordinateur n'a pas réussi à trouver")
-		perdu = True
+    if etat_pendu>=7:
+        perdu = True
+        print("L\'ordinateur n'a pas trouvé, le pendu est mort avant...")
+    if None not in mot_final:#Il y a plus de lettres inconnues dans le mot donc on a gagné
+        print("Trouvé ! Le mot était "+"".join(mot_final))
+        trouve = True
+    if len(liste_mots_possibles)==1 and not trouve:
+        trouve=True
+        print("Trouvé ! Le mot était "+liste_mots_possibles[0])
+    if len(liste_mots_possibles)==0 and trouve==False:#Le mot qu'on cherche n'est visiblement pas dans le dictionnaire, on a donc perdu
+        print("L'ordinateur n'a pas réussi à trouver")
+        perdu = True
