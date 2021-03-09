90b90b4aeea2c5a1a96d37bd1df6acf8358d0bb4
LupyXev <lucien.grancher@protonmail.com>
2021-03-09 11:42:16 +0100

    Début de la recherche du mot par l'utilisateur

--- interface-v2.py
+++ interface-v2.py
@@ -157,6 +157,65 @@ def jeu_solo():
     print("mot choisi")
     clear_window(fenetre, [textCredits])
     #on a fini de choisir le mot
+    play(mot)
+
+
+def play(motADeviner):
+    #boucle principale
+    trouve = False
+    perdu = False
+    etatPendu = 0
+    listeLettresTrouvees = [None]*len(motADeviner)
+    listeLettresDonnees = [] #Liste de toutes les lettres dejà proposées par l'utilisateur
+
+    while trouve == False and perdu == False:
+
+        lettreActu = None
+        while lettreActu in listeLettresDonnees or lettreActu == None: #On vérifie qu'on ne rentre pas 2 fois la même lettre
+            clear_window(fenetre)
+            valide = tk.BooleanVar()
+            boutonValider = tk.Button(fenetre, text="Valider", command=lambda: valide.set(True))
+            boutonValider.grid(row=5)
+            lettreActuEntry = tk.StringVar()
+            valide = tk.BooleanVar()
+            entree=tk.Entry(fenetre, textvariable=lettreActuEntry, width=30)
+            entree.grid(row=2,column=10)
+            boutonValider.wait_variable(valide) #attend la validation des données
+            lettreActu = lettreActuEntry.get().upper()
+
+
+        listeLettresDonnees.append(lettreActu)
+
+        #On regarde si la lettre appartient au mot
+        reponseJuste = False
+        for index in range(len(motADeviner)):
+            if motADeviner[index] == lettreActu:
+                listeLettresTrouvees[index] = lettreActu
+                reponseJuste = True
+
+        if reponseJuste:
+            print("Cette lettre appartient au mot")
+        else:
+            etatPendu += 1
+            print("Cette lettre est incorrecte")
+
+        print(dessinPendu(etatPendu))
+
+        #Affichage du mot dans la console avec des "_" pour les trous
+        for lettre in listeLettresTrouvees:
+            if lettre == None:
+                print("-", end="")
+            else:
+                print(lettre, end="")
+        print("")
+
+        #On regarde si on a gagné ou perdu
+        if etatPendu >= 6:
+            perdu = True
+        elif None not in listeLettresTrouvees:
+            trouve = True
+
+
 
 arborescence = "dico.txt"
 
